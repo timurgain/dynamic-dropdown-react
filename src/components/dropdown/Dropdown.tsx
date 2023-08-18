de6e1865ec0612340f71d0139dcf54c7d172a584
Timur Gain <timur.gain@gmail.com>
2023-08-18 22:03:27 +0400

    feat: hide dropdown if no space

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -16,52 +16,115 @@ export function Dropdown({
   triggerAction,
   open,
 }: DropdownProps): React.JSX.Element {
+  // className state
+  const [menuClass, setMenuClass] = React.useState<string>('dropdown__menu');
+
+  // External open-close control if needed
   const [isOpen, setIsOpen] = React.useState<boolean>(false);
   React.useEffect(() => setIsOpen(open), [open]);
 
-  const [menuClass, setMenuClass] = React.useState<string>('dropdown__menu');
+  // Screen dimensions
+  const [screenWidth, setScreenWidth] = React.useState<number>(0);
+  const [screenHeight, setScreenHeight] = React.useState<number>(0);
+
+  function calcScreenSize() {
+    setScreenWidth(window.innerWidth);
+    setScreenHeight(window.innerHeight);
+  }
+
+  // Trigger dimensions
+  const triggerRef = React.useRef<HTMLButtonElement>(null);
   const [triggerRect, setTriggerRect] = React.useState<{
     [key: string]: number;
   } | null>(null);
-  const triggerRef = React.useRef<HTMLButtonElement>(null);
 
+  function calcTriggerRect() {
+    if (triggerRef.current) {
+      const { top, bottom, left, right, width, height } =
+        triggerRef.current.getBoundingClientRect();
+      setTriggerRect({ top, bottom, left, right, width, height });
+    }
+  }
+
+  // Menu dimensions
+  const menuRef = React.useRef<HTMLElement>(null);
+  const [menuRect, setMenuRect] = React.useState<{
+    [key: string]: number;
+  } | null>(null);
+
+  function calcMenuRect() {
+    if (menuRef.current) {
+      const { top, bottom, left, right, width, height } =
+        menuRef.current.getBoundingClientRect();
+      setMenuRect({ top, bottom, left, right, width, height });
+    }
+  }
+
+  // Listen to scroll and resize then Set dimensions
   React.useEffect(() => {
-    calcTriggerRect();
-    window.addEventListener('scroll', calcTriggerRect);
-    return () => window.removeEventListener('scroll', calcTriggerRect);
+    function calcDimensions() {
+      calcTriggerRect();
+      calcMenuRect();
+      calcScreenSize();
+    }
+    calcDimensions();
+
+    window.addEventListener('scroll', calcDimensions);
+    window.addEventListener('resize', calcDimensions);
+    return () => {
+      window.removeEventListener('scroll', calcDimensions);
+      window.removeEventListener('resize', calcDimensions);
+    };
   }, []);
 
+  // Calculate menu position
   React.useEffect(() => {
     function getOpenDirection() {
       if (triggerRect) {
         const { top, bottom, left, right } = triggerRect;
-        const openX = left >= window.innerWidth - right ? 'left' : 'right';
-        const openY = top >= window.innerHeight - bottom ? 'up' : 'down';
+        const openX = left >= screenWidth - right ? 'left' : 'right';
+        const openY = top >= screenHeight - bottom ? 'up' : 'down';
         return [openY, openX];
       }
       return [];
     }
 
+    function enoughSpace() {
+      if (menuRect && triggerRect) {
+        const { width: menuWidth, height: menuRight } = menuRect;
+        const { width: triggerWidth, height: triggerHeight } = triggerRect;
+        const widthOk = triggerWidth + menuWidth < screenWidth ? true : false;
+        const heightOk =
+          triggerHeight + menuRight < screenHeight ? true : false;
+
+        // console.log('widthOk', triggerWidth + menuWidth, '<', screenWidth, '=', widthOk)
+        // console.log('heightOk', triggerHeight + menuRight, '<', screenHeight, '=', heightOk)
+
+        if (widthOk && heightOk) {
+          return true;
+        }
+      }
+      return false;
+    }
+
     function getClass() {
       const [openY, openX] = getOpenDirection();
-      const cls = ['dropdown__menu'];
-      if (openX && openY) {
-        cls.push(`dropdown__menu_${openY}-${openX}`);
+      const clsBase = 'dropdown__menu';
+      const clsDirection =
+        openX && openY ? `dropdown__menu_${openY}-${openX}` : '';
+      const clsVisiability = enoughSpace() ? '' : 'dropdown__menu_hidden';
+      if (menuRef.current) {
+        return [clsBase, clsDirection, clsVisiability].join(' ');
+      } else {
+        return [clsBase, clsDirection].join(' ');
       }
-      return cls.join(' ');
     }
-
-    setMenuClass(getClass());
-  }, [triggerRect]);
-
-  function calcTriggerRect() {
-    if (triggerRef.current) {
-      const { top, bottom, left, right } =
-        triggerRef.current.getBoundingClientRect();
-      setTriggerRect({ top, bottom, left, right });
+    if (screenWidth > 1) {
+      setMenuClass(getClass());
     }
-  }
+  }, [triggerRect, menuRect, screenWidth, screenHeight]);
 
+  // Open/Close handler
   function handleOpenClose() {
     if (triggerAction) {
       triggerAction();
@@ -81,7 +144,7 @@ export function Dropdown({
       </button>
 
       {isOpen && (
-        <nav className={menuClass}>
+        <nav ref={menuRef} className={menuClass}>
           <Menu items={items} closeMenu={() => handleOpenClose()} />
         </nav>
       )}
